914929b13ca5fafdfa6f8bc294fef3f552439651
myName <myemail@gmail.com>
2023-03-17 16:12:25 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 /**
  * main - prints all single digit numbers of base 10 starting from 0,
  * followed by a new line, using putchar
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
@@ -15,4 +15,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-} 
+}
