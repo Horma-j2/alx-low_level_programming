9b5966974ed233f5eb6cd10fcb9c22d1370d7eaf
myName <myemail@gmail.com>
2023-03-17 15:45:23 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,24 @@
+/*
+ * File: 2-print_alphabet.c
+	 * Prints the alphabet in lowercase
+ */
+
 #include <stdio.h>
+
 /**
- * main - prints the alphabet in lowercase,
- * followed by a new line
- * Return: Always 0 (Success)
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
  */
 int main(void)
-{
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
-{
-putchar(ch);
-}
-putchar('\n');
-return (0);
-}  
+
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++);
+	
+        	putchar(letter);
+
+	        putchar('\n')
+	
+	return (0);
+} 
