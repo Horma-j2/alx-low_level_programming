040caa48700c624f0efc0a04d06912133d66a714
myName <myemail@gmail.com>
2023-03-22 14:18:11 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * Main- 1-alphabet.c
- * print_alphabet - prints alphabet in lower case followed by a new line
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line
  */
+
 void print_alphabet(void)
 {
 	char one;
 
 	for (one = 'a'; one <= 'z'; one++)
 		_putchar(one);
+
 	_putchar('\n');
 }
