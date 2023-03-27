81b749f271cb71aae461d99bb5c895c6e167fab5
myName <myemail@gmail.com>
2023-03-27 06:15:18 -0700

    main

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+
+/**
+ * File - main.h
+ * Desc - Header files for declaration of all functions
+ *        Used in the 0x05-pointers_arrays_strings directory
+ * Auth - Horma-j2
+ */
+
+#endif
