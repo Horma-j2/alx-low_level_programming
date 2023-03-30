684727698c6f2f75e3cc0c5ae5c3d867806e06a4
myName <myemail@gmail.com>
2023-03-30 10:28:32 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - to concatenate a string into another string
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: concatenated value
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *dest = 'i';
+	char *src = 'j';
+	{
+		for ( i = 0; i != '\0'; );
+		i++;
+	}
+	{
+		while ( j != '\0'; j++; );
+		i = j;
+		i++;
+	}
+
+	return (i)('\n');
+}
