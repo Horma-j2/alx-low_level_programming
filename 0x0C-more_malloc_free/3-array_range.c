2b2dd6683d1f06d901c5ee0a4ca3b349fc503c0c
myName <myemail@gmail.com>
2023-04-14 10:05:16 -0700

    array_range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers
+ * @min: minimum value
+ * @max: maximum value
+ * Return: array
+ */
+
+int *array_range(int min, int max)
+{
+	int i, *arr, j = max - min + 1;
+
+	if (min > max)
+		return (NULL);
+
+	arr = malloc(j * sizeof(*arr));
+
+	if (arr == 0)
+		return (NULL);
+
+	for (i = 0; i < j; i++)
+	{
+		arr[i] = min++;
+	}
+
+	return (arr);
+}
