1bacf832a22822c8e49c7dc746196aab6849bd10
Horma-j2 <ogundairoabiola5@gmail.com>
2023-04-27 10:12:29 -0700

    list

---
+++ 0x12-singly_linked_lists/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stand out
+ * @c: the character to stand out
+ * Return: if successful 1
+ * On error: -1 is returned and error is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
